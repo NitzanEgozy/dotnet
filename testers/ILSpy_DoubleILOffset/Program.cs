1f3ad97ac0eee0f856f10a6d6e0ea6807839034e
Nitzan Egozy <nitzan.egozy@overops.com>
2019-09-24 09:33:22 +0300

    ILSpy DoubleILOffset bug tester

---
+++ testers/ILSpy_DoubleILOffset/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleILOffset
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				reThrowException();
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine(e);
+			}
+		}
+
+		public static void throwException()
+		{
+			throw new Exception();
+		}
+		private static void reThrowException()
+		{
+			try
+			{
+				throwException();
+			}
+			catch (Exception e)
+			{
+				throw e;
+			}
+		}
+	}
+}
