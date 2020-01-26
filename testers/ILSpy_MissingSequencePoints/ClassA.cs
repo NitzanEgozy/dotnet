5877c6a644d25174b1770372d1ffcdfb62bd9cd8
Nitzan Egozy <nitzan.egozy@overops.com>
2020-01-26 14:20:51 +0200

    Added ILSpy_MissingSequencePoints tester

---
+++ testers/ILSpy_MissingSequencePoints/ClassA.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security;
+
+namespace MissingSequencePoints
+{
+	public class ClassA
+	{
+		public void func1()
+		{
+			try
+			{
+				throw new Exception();
+			}
+			catch (Exception)
+			{
+			}
+		}
+	}
+}
---
+++ testers/ILSpy_MissingSequencePoints/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MissingSequencePoints
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			new ClassA().func1();
+			Console.WriteLine("End of test.");
+		}
+	}
+}
